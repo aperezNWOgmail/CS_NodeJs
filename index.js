bc38b89ca75ecca330b19ad52e7e8e0253493906
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2023-12-01 23:04:40 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-console.log("mcsd");
+//
+console.log("[APPLICATION START]");
 
+//
+function SudokuGenerate() {
+  return "[{2,0,8,5,1,3,4,6,7},{0,3,6,2,4,0,1,9,8},{0,0,0,8,6,0,2,5,3},{3,2,5,4,7,6,0,1,9},{0,6,9,1,8,2,3,7,5},{8,7,0,3,0,5,0,0,2},{0,4,7,9,2,8,0,3,1},{1,8,0,7,5,4,0,2,6},{9,5,2,0,0,1,7,0,4}]";
+}
+
+//
 const express = require("express");
+const cors = require("cors");
+
+//
 const app = express();
+
+//
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 // Handling GET requests for different endpoints
 app.get("/Sudoku_Generate_NodeJS", (req, res) => {
-  res.send(
-    "[{2,0,8,5,1,3,4,6,7},{0,3,6,2,4,0,1,9,8},{0,0,0,8,6,0,2,5,3},{3,2,5,4,7,6,0,1,9},{0,6,9,1,8,2,3,7,5},{8,7,0,3,0,5,0,0,2},{0,4,7,9,2,8,0,3,1},{1,8,0,7,5,4,0,2,6},{9,5,2,0,0,1,7,0,4}]",
-  );
+  res.send(SudokuGenerate());
 });
 
+//
 app.get("/endpoint2", (req, res) => {
   res.send("Response for endpoint2");
 });
