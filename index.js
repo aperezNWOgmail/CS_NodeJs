5529650e8612b4072e30eacb4b09f1a6ddcb1ddb
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2023-12-01 22:05:36 +0000

    Update 6 files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("mcsd");
+
+const express = require("express");
+const app = express();
+
+// Handling GET requests for different endpoints
+app.get("/endpoint1", (req, res) => {
+  res.send("Response for endpoint1");
+});
+
+app.get("/endpoint2", (req, res) => {
+  res.send("Response for endpoint2");
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
