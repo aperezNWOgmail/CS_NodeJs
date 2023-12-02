b7a0aa147cb25686601f4c24cc4cae630e4842d7
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2023-12-02 02:09:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,45 +5,42 @@ function SudokuGenerate() {
   return "[{2,0,8,5,1,3,4,6,7},{0,3,6,2,4,0,1,9,8},{0,0,0,8,6,0,2,5,3},{3,2,5,4,7,6,0,1,9},{0,6,9,1,8,2,3,7,5},{8,7,0,3,0,5,0,0,2},{0,4,7,9,2,8,0,3,1},{1,8,0,7,5,4,0,2,6},{9,5,2,0,0,1,7,0,4}]";
 }
 //
-function DatabaseConnect() {
-  //
-  let _result = "[]";
-  let result = "";
+async function DatabaseConnect() {
   //
   const sql = require("mssql");
   //
-  (async () => {
-    //
-    const config = {
-      user: "aperezNWO_SQLLogin_1",
-      password: "aperezNWO_SQLLogin_1",
-      server: "webapiangulardemo.mssql.somee.com",
-      database: "webapiangulardemo",
-      options: {
-        encrypt: true,
-        trustServerCertificate: true,
-      },
-    };
-    //
-    try {
-      const pool = await sql.connect(config);
-      result = await pool.request().query("SELECT * FROM PERSONA");
-      console.dir(result);
-      _result = "[CONNECTION OK]";
-      //
-    } catch (err) {
-      console.error("Error:", err);
-      if (err.originalError && err.originalError.info) {
-        console.error("Detailed Error Info:", err.originalError.info);
-      }
-      _result = "[CONNECTION ERROR]";
-      //
-    } finally {
-      sql.close();
-    }
-  })();
+  const config = {
+    user: "aperezNWO_SQLLogin_1",
+    password: "aperezNWO_SQLLogin_1",
+    server: "webapiangulardemo.mssql.somee.com",
+    database: "webapiangulardemo",
+    options: {
+      encrypt: true,
+      trustServerCertificate: true,
+    },
+  };
   //
-  return _result;
+  try {
+    //
+    const pool   = await sql.connect(config);
+    const result = await pool.request().query("SELECT * FROM PERSONA");
+    //
+    console.log(result);
+    //
+    return result;
+  } catch (err) {
+    //
+    console.error("Error:", err);
+    //
+    if (err.originalError && err.originalError.info) {
+      console.error("Detailed Error Info:", err.originalError.info);
+    }
+    //
+    return JSON.parse("[]");
+    //
+  } finally {
+    sql.close();
+  }
 }
 
 //
@@ -64,9 +61,11 @@ app.get("/Sudoku_Generate_NodeJS", (req, res) => {
 });
 
 //
-app.get("/DatabaseConnect", (req, res) => {
-  res.send(DatabaseConnect());
-});
+(async () => {
+  app.get("/DatabaseConnect", (req, res) => {
+    res.send(await DatabaseConnect());
+  });
+})();
 
 // Start the server on port 3000
 app.listen(3000, () => {
